6ebd120400ba0803390a0b4d676d4f356dd4f087
Bob <48901347+BobTheSoftwareDeveloper@users.noreply.github.com>
2021-05-03 20:22:01 +1200

    AS-7 Update API doc endpoint URL

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = config.port || 5000
 
 const app = express()
 app.use(express.json())
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpecs))
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpecs))
 app.use("", checkFirebaseToken)
 
 if (config.NODE_ENV !== "production") {
