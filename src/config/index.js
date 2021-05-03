b3fd80c3197549b9eb11d9ecb058eddf607fef23
Bob <48901347+BobTheSoftwareDeveloper@users.noreply.github.com>
2021-05-04 01:20:20 +1200

    Update dotenv config

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,7 @@
 const dotenv = require('dotenv');
 const path = require("path")
 
-const envFound = dotenv.config({
-  path: path.resolve(__dirname, "../../", ".env")
-});
+const envFound = dotenv.config()
 if (envFound.error) {
   throw new Error("⚠️  Couldn't find .env file  ⚠️");
 }
